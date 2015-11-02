555de2b69f3e7fce3a3f1db201503ed4b3fb6cc7
Justin Burris <justinbburris@gmail.com>
2015-11-01 22:17:12 -0500

    Display groups available for a given hue network

---
+++ tinct/tinct.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
+
+
+class LightGroupList(BoxLayout):
+    from hue import Hue
+    h = Hue('un', 'ip')
+    light_groups = ListProperty([group['name'] for group in h.groups()['resource']])
+
+class Tinct(Widget):
+    pass
+
+
+class TinctApp(App):
+    def build(self):
+        return Tinct()
+
+if __name__ == '__main__':
+    TinctApp().run()
