39d31506cfb56c76a5a6c6646bf771d25399d000
Justin Burris <justinbburris@gmail.com>
2015-11-22 12:29:59 -0500

    Display the lights for the currently selected group

--- tinct/tinct.py
+++ tinct/tinct.py
@@ -6,12 +6,27 @@ from kivy.properties import ListProperty, ObjectProperty
 from hue import Hue
 import settings
 
+class GroupControlPanel(BoxLayout):
+    hue_bridge = ObjectProperty(None)
+    selected_group = ObjectProperty(None)
+    lights = ListProperty([])
+
+    def on_selected_group(self, instance, pos):
+        group_id = int(self.selected_group['id'])
+        group = self.hue_bridge.groups(group_id)
+        lights = [self.hue_bridge.lights(int(light_id)) for light_id in group['lights']]
+        self.lights = [light['name'] for light in lights]
+
 class LightGroupPanel(BoxLayout):
     hue_bridge = ObjectProperty(None)
+    selected_group = ObjectProperty(None)
     light_groups = ListProperty([])
 
     def on_hue_bridge(self, instance, pos):
-        self.light_groups = [group['name'] for group in self.hue_bridge.groups()]
+        groups = self.hue_bridge.groups()
+
+        self.light_groups = [group['name'] for group in groups]
+        self.selected_group = groups[0]
 
 class Tinct(Widget):
 
